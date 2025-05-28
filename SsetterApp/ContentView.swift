38427be2e06ab7b20bc69ba79b28dcec922bb3f7
jjehyun <povt99@gmail.com>
2025-05-28 09:48:32 +0900

    Initial Commit

---
+++ SsetterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SsetterApp
+//
+//  Created by anonymous on 5/28/R7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SsetterApp/SsetterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SsetterAppApp.swift
+//  SsetterApp
+//
+//  Created by anonymous on 5/28/R7.
+//
+
+import SwiftUI
+
+@main
+struct SsetterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
